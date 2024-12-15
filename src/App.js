8718e5540a4428180b58731a16888179cfbe42ff
unknown <mateusbeltrao2019@gmail.com>
2024-12-15 08:12:47 -0300

    skill

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Banner } from './components/Banner';
 import { NavBar } from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Skills } from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Banner></Banner>
+      <Skills></Skills>
     </div>
   );
 }
